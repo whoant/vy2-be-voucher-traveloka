b0d14217df3e63d353538a6fdbdc992fef573740
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-17 15:32:43 +0700

    Fix bug

--- src/helpers/response.helper.js
+++ src/helpers/response.helper.js
@@ -1,35 +1,35 @@
 const sendErrorDev = (err, res) => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-        error: err,
-        stack: err.stack
-    });
+	res.status(err.statusCode || 500).json({
+		status: err.status,
+		message: err.message,
+		error: err,
+		stack: err.stack
+	});
 };
 
 const sendErrorProd = (err, res) => {
-    if (err.isOperational) {
-        res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message
-        });
-    } else {
-        console.error('ERROR 💥', err);
-        res.status(500).json({
-            status: 'error',
-            message: 'Something went very wrong !'
-        });
-    }
+	if (err.isOperational) {
+		res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message
+		});
+	} else {
+		console.error('ERROR 💥', err);
+		res.status(500).json({
+			status: 'error',
+			message: 'Something went very wrong !'
+		});
+	}
 }
 
 const handleError = (error, req, res, next) => {
-    if (process.env.NODE_ENV === 'development') {
-        sendErrorDev(error, res);
-    } else {
-        sendErrorProd(error, res)
-    }
+	if (process.env.NODE_ENV === 'development') {
+		sendErrorDev(error, res);
+	} else {
+		sendErrorProd(error, res)
+	}
 };
 
 module.exports = {
-    handleError
+	handleError
 };

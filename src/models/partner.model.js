83eb9fb7d85d06ab12627f213afcde7847940617
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-17 15:59:35 +0700

    Add hook

--- src/models/partner.model.js
+++ src/models/partner.model.js
@@ -1,25 +1,33 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const { sha256 } = require("../helpers/hash.helper");
 
 module.exports = (sequelize) => {
-    return sequelize.define('Partner', {
-        id: {
-            type: DataTypes.UUID,
-            allowNull: false,
-            primaryKey: true,
-            defaultValue: Sequelize.UUIDV4,
-        },
-        username: {
-            type: DataTypes.STRING,
-            unique: true,
-            allowNull: false,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        secretKey: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-    });
+	return sequelize.define('Partner', {
+		id: {
+			type: DataTypes.UUID,
+			allowNull: false,
+			primaryKey: true,
+			defaultValue: Sequelize.UUIDV4,
+		},
+		username: {
+			type: DataTypes.STRING,
+			unique: true,
+			allowNull: false,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		secretKey: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true
+		},
+	}, {
+		hooks: {
+			beforeCreate(record, options) {
+				record.dataValues.password = sha256(record.dataValues.password);
+			}
+		}
+	});
 };

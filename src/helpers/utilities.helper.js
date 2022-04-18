58a4de651fc9ccdfa53d6d28a3e2aa9849087a75
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-19 00:34:48 +0700

    Add format money in VN

--- src/helpers/utilities.helper.js
+++ src/helpers/utilities.helper.js
@@ -3,3 +3,9 @@ const moment = require('moment');
 exports.compareDate = (firstDate, secondDate) => {
 	return moment(firstDate).isBefore(secondDate);
 };
+
+exports.formatMoney = (money) => {
+	return money.split('').reverse().reduce((prev, next, index) => {
+		return ((index % 3) ? next : (next + ',')) + prev
+	});
+};

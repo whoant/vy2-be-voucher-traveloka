03bbf286dd11011b84eece4e13bedd0dc79dba6f
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-18 22:01:07 +0700

    Add route voucher

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const authRoute = require('./auth.route');
+const voucherRoute = require('./voucher.route');
 
 module.exports = app => {
-    app.use('/api/v1/auth', authRoute);
+	app.use('/api/v1/auth', authRoute);
+	app.use('/api/v1/partner/voucher', voucherRoute);
 };

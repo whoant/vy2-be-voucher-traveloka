3d842dabaf2322be883fd36d1485ce5a8cbf960c
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-19 02:19:09 +0700

    Add func random n characters

--- src/helpers/utilities.helper.js
+++ src/helpers/utilities.helper.js
@@ -9,3 +9,15 @@ exports.formatMoney = (money) => {
 		return ((index % 3) ? next : (next + ',')) + prev
 	});
 };
+
+exports.randomString = length => {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = '';
+	const charactersLength = characters.length;
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() *
+			charactersLength));
+	}
+
+	return result;
+};

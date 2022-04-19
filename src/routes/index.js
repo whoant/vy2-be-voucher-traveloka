0520d2df5e5996183c88101583c89431eab01362
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-19 08:33:06 +0700

    gift card route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const authRoute = require('./auth.route');
 const voucherRoute = require('./voucher.route');
+const giftCardRoute = require('./giftCard.route');
 
 module.exports = app => {
 	app.use('/api/v1/auth', authRoute);
 	app.use('/api/v1/partner/voucher', voucherRoute);
+	app.use('/api/v1/partner/gift-card', giftCardRoute);
 };

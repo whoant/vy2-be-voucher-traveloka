fd5d8f9df8258bedb4a2e2808bc0edbb7fbf3537
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-19 08:40:10 +0700

    Refactor code

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const authController = require('../controllers/auth.controller');
+const authController = require('../controllers/partner/auth.controller');
 const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post('/login', authMiddleware.validatePartner, authController.login);

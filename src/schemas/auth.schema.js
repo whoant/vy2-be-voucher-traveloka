e48a89b463cc34456c6b728b92d50cca1e9a1bd7
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-24 21:55:40 +0700

    Add user schema

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -2,8 +2,15 @@ const yup = require("yup");
 
 exports.authSchema = yup.object({
 	body: yup.object({
-		username: yup.string().required(),
-		password: yup.string().required(),
+		username: yup.string().required("Vui lòng điền username !"),
+		password: yup.string().required("Vui lòng điền password !"),
 		// secretKey: yup.string().required(),
 	})
 });
+
+exports.userSchema = yup.object({
+	body: yup.object({
+		userId: yup.string().required("Vui lòng điền userId !"),
+		email: yup.string().email("Vui lòng nhậpp đúng định dạng mail !").required("Vui lòng điền email !")
+	})
+})

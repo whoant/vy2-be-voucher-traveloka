9ca0a5f80d25f54d4f4913701f9da64f62714bae
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-25 20:05:55 +0700

    Add user voucher route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const voucherPartnerRoute = require('./voucherPartner.route');
 const giftCardPartnerRoute = require('./giftCardPartner.route');
 
 const authUserRoute = require('./authUser.route');
+const voucherUserRoute = require('./voucherUser.route');
 
 module.exports = app => {
-	app.use('/api/v1/partner/auth', authPartnerRoute);
-	app.use('/api/v1/partner/voucher', voucherPartnerRoute);
-	app.use('/api/v1/partner/gift-card', giftCardPartnerRoute);
+    app.use('/api/v1/partner/auth', authPartnerRoute);
+    app.use('/api/v1/partner/voucher', voucherPartnerRoute);
+    app.use('/api/v1/partner/gift-card', giftCardPartnerRoute);
 
-	app.use('/api/v1/user/auth', authUserRoute);
+    app.use('/api/v1/user/auth', authUserRoute);
+    app.use('/api/v1/user/voucher', voucherUserRoute);
 };

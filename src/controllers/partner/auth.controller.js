0b24071a3d1791ffb4b84c9f7eb208e74a0f37dc
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-25 20:05:25 +0700

    Auth controller

--- src/controllers/partner/auth.controller.js
+++ src/controllers/partner/auth.controller.js
@@ -4,35 +4,35 @@ const catchAsync = require('../../helpers/catchAsync.helper');
 const { Partner } = require("../../models");
 
 exports.login = catchAsync(async (req, res) => {
-	const { username, password } = req.body;
-	const partner = await Partner.findOne({
-		where: {
-			username, password
-		}
-	});
+    const { username, password } = req.body;
+    const partner = await Partner.findOne({
+        where: {
+            username, password
+        }
+    });
 
-	if (!partner) throw new AppError('Partner không tồn tại !');
-	const token = await generateToken(partner.id);
+    if (!partner) throw new AppError('Partner không tồn tại !');
+    const token = await generateToken(partner.id);
 
-	res.json({
-		status: 'success',
-		message: 'Đăng nhập thành công !',
-		data: {
-			token
-		}
-	});
+    res.json({
+        status: 'success',
+        message: 'Đăng nhập thành công !',
+        data: {
+            token
+        }
+    });
 });
 
 exports.createPartner = catchAsync(async (req, res) => {
-	const { username, password, secretKey } = req.body;
-	if (!secretKey) throw new AppError('Vui lòng kiểm tra lại tài khoản của mình !', 400);
-	await Partner.create({
-		username, password, secretKey
-	});
+    const { username, password, secretKey, typeVoucher } = req.body;
+    if (!secretKey) throw new AppError('Vui lòng kiểm tra lại tài khoản của mình !', 400);
+    await Partner.create({
+        username, password, secretKey, typeVoucher
+    });
 
-	res.json({
-		status: 'success',
-		message: 'Tạo partner thành công !',
-	});
+    res.json({
+        status: 'success',
+        message: 'Tạo partner thành công !',
+    });
 });
 

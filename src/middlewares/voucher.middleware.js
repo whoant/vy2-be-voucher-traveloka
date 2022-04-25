43857702f905ab3bee36dd919b8c6c529138e8ac
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-25 23:19:12 +0700

    Format voucher middleware

--- src/middlewares/voucher.middleware.js
+++ src/middlewares/voucher.middleware.js
@@ -3,10 +3,10 @@ const catchAsync = require('../helpers/catchAsync.helper');
 const AppError = require("../helpers/appError.helper");
 
 exports.validateVoucher = catchAsync(async (req, res, next) => {
-	try {
-		await voucherSchema.validate(req);
-		return next();
-	} catch (e) {
-		throw new AppError('Dữ liệu không hợp lệ !', 400);
-	}
+    try {
+        await voucherSchema.validate(req);
+        return next();
+    } catch (e) {
+        throw new AppError('Dữ liệu không hợp lệ !', 400);
+    }
 });

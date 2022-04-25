ffc7076fdbb77747cc403c796f54cb4e1c720aa4
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-25 21:16:15 +0700

    Parse number

--- src/helpers/combineDescription.helper.js
+++ src/helpers/combineDescription.helper.js
@@ -1,21 +1,21 @@
 const { formatMoney } = require("./utilities.helper");
 
 exports.combineDescriptionVoucher = (condition) => {
-	const { threshold, discount, maxAmount } = condition;
-	const formatThreshold = formatMoney(threshold);
-	const formatMaxAmount = formatMoney(maxAmount);
-	if (discount === 0) {
-		return `Đơn hàng trị giá trên ${formatThreshold}đ sẽ nhận được giảm giá ${formatMaxAmount}đ`;
-	}
-	return `Đơn hàng trị giá trên ${formatThreshold}đ sẽ nhận được giảm giá ${discount}%, không vượt quá ${formatMaxAmount}đ`;
+    const { threshold, discount, maxAmount } = condition;
+    const formatThreshold = formatMoney(threshold);
+    const formatMaxAmount = formatMoney(maxAmount);
+    if (Number(discount) === 0) {
+        return `Đơn hàng trị giá trên ${formatThreshold}đ sẽ nhận được giảm giá ${formatMaxAmount}đ`;
+    }
+    return `Đơn hàng trị giá trên ${formatThreshold}đ sẽ nhận được giảm giá ${discount}%, không vượt quá ${formatMaxAmount}đ`;
 }
 
 exports.combineDescriptionGiftCard = (giftCard) => {
-	const { discount, typeGift } = giftCard;
+    const { discount, typeGift } = giftCard;
 
-	if (typeGift === 'PERCENT') {
-		return `Đơn hàng sẽ được giảm giá ${discount}%`;
-	}
+    if (typeGift === 'PERCENT') {
+        return `Đơn hàng sẽ được giảm giá ${discount}%`;
+    }
 
-	return `Đơn hàng sẽ được giảm giá ${formatMoney(discount)} đ`;
+    return `Đơn hàng sẽ được giảm giá ${formatMoney(discount)} đ`;
 }

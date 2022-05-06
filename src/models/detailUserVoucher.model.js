c32fd5b5dd7ab2d96201b68ef598dd941f8e4a15
whoant <vovanhoangtuan4.2@gmail.com>
2022-05-06 07:20:44 +0700

    Add usedAt to DetailUserVoucher

--- src/models/detailUserVoucher.model.js
+++ src/models/detailUserVoucher.model.js
@@ -1,37 +1,42 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-	return sequelize.define('DetailUserVoucher', {
-		id: {
-			type: DataTypes.UUID,
-			allowNull: false,
-			primaryKey: true,
-			defaultValue: Sequelize.UUIDV4,
-		},
-		transactionId: {
-			type: DataTypes.STRING,
-			unique: true,
-			allowNull: false,
-		},
-		amount: {
-			type: DataTypes.BIGINT,
-			allowNull: false,
-			validate: {
-				isInt: {
-					args: true,
-					msg: 'Số tiền không hợp lệ !'
-				}
-			}
-		},
-		amountAfter: {
-			type: DataTypes.BIGINT,
-			allowNull: false,
-			validate: {
-				isInt: {
-					args: true,
-					msg: 'Số tiền sau khi giảm không hợp lệ !'
-				}
-			}
-		},
-	});
+    return sequelize.define('DetailUserVoucher', {
+        id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: Sequelize.UUIDV4,
+        },
+        transactionId: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        amount: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            validate: {
+                isInt: {
+                    args: true,
+                    msg: 'Số tiền không hợp lệ !'
+                }
+            }
+        },
+        amountAfter: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            validate: {
+                isInt: {
+                    args: true,
+                    msg: 'Số tiền sau khi giảm không hợp lệ !'
+                }
+            }
+        },
+        usedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.NOW
+        }
+    });
 };

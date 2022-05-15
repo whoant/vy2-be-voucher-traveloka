46dccb0b22ff2ced72a5801388e0d9a6f5b9cf01
whoant <vovanhoangtuan4.2@gmail.com>
2022-05-15 22:18:49 +0700

    Add new table

---
+++ src/models/typeVoucher.model.js
@@ -0,0 +1,33 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const TypeVoucher = sequelize.define('TypeVoucher', {
+        id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: Sequelize.UUIDV4,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: {
+                args: true,
+                msg: 'Mã bí mật đã tồn tại !'
+            }
+        },
+        type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: {
+                args: true,
+                msg: 'Loại voucher này đã tồn tại !'
+            }
+        }
+    }, {
+        hooks: {}
+    });
+
+
+    return TypeVoucher;
+};

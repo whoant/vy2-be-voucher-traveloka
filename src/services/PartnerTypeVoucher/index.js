b8a77709edaa5bfbad9608dfb83519353523341b
whoant <vovanhoangtuan4.2@gmail.com>
2022-05-16 00:18:50 +0700

    PartnerTypeVoucherService

---
+++ src/services/PartnerTypeVoucher/index.js
@@ -0,0 +1,46 @@
+const { PartnerTypeVoucher, TypeVoucher } = require("../../models");
+const AppError = require("../../helpers/appError.helper");
+
+class PartnerTypeVoucherService {
+    constructor(partnerVoucher) {
+        this.partnerVoucher = partnerVoucher;
+    }
+
+    async find(partnerId, type) {
+        const typeVoucher = await this.findTypeVoucher(type);
+
+        const partnerVoucher = await PartnerTypeVoucher.findOne({
+            where: {
+                partnerId,
+                typeVoucherId: typeVoucher.id
+            }
+        });
+
+        if (!partnerVoucher) throw new AppError('Partner không tồn tại !');
+        this.partnerVoucher = partnerVoucher;
+
+        return partnerVoucher;
+    }
+
+    async createVoucher(voucher) {
+        return this.partnerVoucher.createVoucher(voucher);
+    }
+
+    async findTypeVoucher(type) {
+        const typeVoucher = await TypeVoucher.findOne({
+            where: {
+                type
+            }
+        });
+        if (!typeVoucher) throw new AppError('Loại voucher không tồn tại !');
+
+        return typeVoucher;
+    }
+
+    getId() {
+        return this.partnerVoucher.id;
+    }
+
+}
+
+module.exports = PartnerTypeVoucherService;

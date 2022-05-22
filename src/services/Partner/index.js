d195050c58fb06f7585d8d513fa8eddc30f17c82
whoant <vovanhoangtuan4.2@gmail.com>
2022-05-23 01:01:04 +0700

    Add getDetail func

--- src/services/Partner/index.js
+++ src/services/Partner/index.js
@@ -5,26 +5,27 @@ const {
     PartnerTypeVoucher,
     TypeVoucher,
     UserVoucher,
-    DetailUserVoucher, User
+    DetailUserVoucher, User, sequelize
 } = require("../../models");
 const { combineDescriptionGiftCard, combineDescriptionVoucher } = require("../../helpers/combineDescription.helper");
 const PartnerTypeVoucherService = require("../PartnerTypeVoucher");
 const { Op } = require("sequelize");
 const { STATE_PROMOTION } = require("../../constants");
+const VoucherService = require("../voucher");
 
 class PartnerService {
 
-    constructor(partner) {
+    constructor(partner, type) {
         this.partner = partner;
+        this.type = type;
     }
 
     getPartnerId() {
         return this.partner.dataValues.id;
     }
 
-    async getVouchers(type) {
-        const partnerVoucher = new PartnerTypeVoucherService(null);
-        await partnerVoucher.find(this.getPartnerId(), type);
+    async getVouchers() {
+        const partnerVoucher = await this.getPartner();
         const vouchers = await Voucher.findAll({
             where: {
                 PartnerTypeVoucherId: partnerVoucher.getId(),
@@ -49,8 +50,7 @@ class PartnerService {
     }
 
     async createVoucher(voucher) {
-        const partnerVoucher = new PartnerTypeVoucherService(null);
-        await partnerVoucher.find(this.getPartnerId(), voucher.type);
+        const partnerVoucher = await this.getPartner();
         const newVoucher = await partnerVoucher.createVoucher(voucher);
 
         return newVoucher.createCondition(voucher);
@@ -105,30 +105,14 @@ class PartnerService {
         return listTypeVoucher;
     }
 
-    async getDetail({ type, code }) {
-        const partnerVoucher = new PartnerTypeVoucherService(null);
-        await partnerVoucher.find(this.getPartnerId(), type);
-        const voucher = await Voucher.findOne({
-            where: {
-                PartnerTypeVoucherId: partnerVoucher.getId(),
-                voucherCode: code
-            },
-            raw: true,
-            nest: true
-        });
+    async getDetail(code) {
+        const partnerVoucher = await this.getPartner();
+        const Voucher = new VoucherService(partnerVoucher);
+        const voucher = await Voucher.getVoucherFromCode(code);
 
-        const userVouchers = await UserVoucher.findAll({
-            where: {
-                voucherId: voucher.id,
-                state: STATE_PROMOTION.DONE
-            },
-            include: {
-                model: DetailUserVoucher,
-                attributes: ['transactionId', 'amount', 'amountAfter', 'usedAt']
-            },
-            raw: true,
-            nest: true
-        });
+        const attributes = ['transactionId', 'amount', 'amountAfter', 'usedAt'];
+
+        const userVouchers = await this.getUserVoucherDone(voucher, attributes);
 
         const listUser = await Promise.all(userVouchers.map(userVoucher => {
             return User.findByPk(userVoucher.userId, {
@@ -151,8 +135,47 @@ class PartnerService {
                 usedAt
             });
         });
-        
+
         return result;
+    }
+
+    async getAnalyzeVoucher(code) {
+        const partnerVoucher = await this.getPartner();
+        const Voucher = new VoucherService(partnerVoucher);
+        const voucher = await Voucher.getVoucherFromCode(code);
+
+        const userVouchers = await this.getUserVoucherDone(voucher, ['amountAfter']);
+
+        const totalAmount = userVouchers.reduce((previousValue, currentValue) => {
+            return previousValue + Number(currentValue.DetailUserVoucher.amountAfter);
+        }, 0);
+
+        return {
+            totalAmount,
+            totalUse: userVouchers.length,
+        };
+
+    }
+
+    async getPartner() {
+        const partnerVoucher = new PartnerTypeVoucherService(null);
+        await partnerVoucher.find(this.getPartnerId(), this.type);
+        return partnerVoucher;
+    }
+
+    async getUserVoucherDone(voucher, attributes) {
+        return UserVoucher.findAll({
+            where: {
+                voucherId: voucher.id,
+                state: STATE_PROMOTION.DONE
+            },
+            include: {
+                model: DetailUserVoucher,
+                attributes
+            },
+            raw: true,
+            nest: true
+        });
     }
 
 }

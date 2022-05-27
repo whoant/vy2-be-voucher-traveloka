af93b0354d491b8f11ef895e0e35cd38cda90abd
whoant <vovanhoangtuan4.2@gmail.com>
2022-05-27 23:43:50 +0700

    Add handleError

--- src/helpers/response.helper.js
+++ src/helpers/response.helper.js
@@ -1,35 +1,44 @@
-const sendErrorDev = (err, res) => {
-	res.status(err.statusCode || 500).json({
-		status: err.status,
-		message: err.message,
-		error: err,
-		stack: err.stack
-	});
+const handleError = require('./handleError.helper');
+
+const sendErrorDev = (err, req, res) => {
+    res.status(err.statusCode || 500).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack
+    });
 };
 
-const sendErrorProd = (err, res) => {
-	if (err.isOperational) {
-		res.status(err.statusCode).json({
-			status: err.status,
-			message: err.message
-		});
-	} else {
-		console.error('ERROR 💥', err);
-		res.status(500).json({
-			status: 'error',
-			message: 'Something went very wrong !'
-		});
-	}
+const sendErrorProd = (err, req, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else {
+        console.error('ERROR 💥', err);
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong !'
+        });
+    }
 }
 
-const handleError = (error, req, res, next) => {
-	if (process.env.NODE_ENV === 'development') {
-		sendErrorDev(error, res);
-	} else {
-		sendErrorProd(error, res)
-	}
-};
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
 
-module.exports = {
-	handleError
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, req, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        let error = { ...err };
+
+        error.message = err.message;
+
+        if (error.name === 'SequelizeUniqueConstraintError') error = handleError.handleUniqueErrorDB(error);
+
+        sendErrorProd(error, req, res);
+    }
+    
+    next();
 };

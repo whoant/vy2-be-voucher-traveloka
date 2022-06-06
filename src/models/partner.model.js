cb81576c6491cefa03bd2c902bc1017ad55d2465
whoant <vovanhoangtuan4.2@gmail.com>
2022-06-06 16:29:57 +0700

    Add app id to model

--- src/models/partner.model.js
+++ src/models/partner.model.js
@@ -1,6 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const { sha256 } = require("../helpers/hash.helper");
 const { randomString } = require("../helpers/utilities.helper");
+const { APP_ID } = require("../constants");
 
 module.exports = (sequelize) => {
     return sequelize.define('Partner', {
@@ -42,7 +43,18 @@ module.exports = (sequelize) => {
                 msg: 'Mã bí mật đã tồn tại !'
             }
         },
+        appId: {
+            allowNull: false,
+            type: DataTypes.ENUM,
+            values: Object.values(APP_ID),
+        }
     }, {
+        indexes: [
+            {
+                unique: true,
+                fields: ['email', 'username']
+            }
+        ],
         hooks: {
             beforeCreate(record, options) {
                 record.dataValues.secretKey = randomString(10);

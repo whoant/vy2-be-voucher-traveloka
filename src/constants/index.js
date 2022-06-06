bed34bafda9eb0c1606f3e1f3ec7ad4a5cde07cd
whoant <vovanhoangtuan4.2@gmail.com>
2022-06-06 16:27:04 +0700

    Add app id

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,5 +12,11 @@ module.exports = {
         "CAR-RENTAL": "CARRENTAL",
         "TOUR": "XPERIENCE",
         "SAVING-COMBO": "COMBO"
+    },
+    APP_ID: {
+        'vy01': 'vy01',
+        'vy02': 'vy02',
+        'vy03': 'vy03',
+        'vy04': 'vy04'
     }
 };
